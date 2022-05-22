fa2618765cd49d835fdba1ff4fd5f435689adb4d
Aldo Lamas <aldolamas01@gmail.com>
2022-05-22 17:04:27 -0500

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let taskForm = document.getElementById('create-task-form');
+  taskForm.addEventListener('submit', submitFunc);
 });
+
+function submitFunc(e) {
+  e.preventDefault();
+
+  let taskList = document.getElementById('tasks');
+  let task = document.createElement('li');
+  task.textContent = e.target['new-task-description'].value;
+  let taskText = e.target['new-task-description'].value
+  task.textContent = `${taskText} `
+  taskList.append(task);
+
+  let btn = document.createElement('button');
+  btn.textContent = 'x';
+  btn.addEventListener('click', handleDelete);
+  task.append(btn);
+
+  e.target.reset();
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
